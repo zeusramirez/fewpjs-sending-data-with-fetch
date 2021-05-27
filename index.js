9ab976f83ebc647039ab12836716cbd084cd1cf1
zeus <zeus.ramirez347@gmail.com>
2021-05-26 22:15:39 -0400

    commit tree 7fe4bf1bee1addb969452f48eecccb72db659fc3

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your code here
+
+
+
+
+function submitData(name, email){
+return fetch('http://localhost:3000/users',{
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        //user data
+        name: `${name}`,
+        email: `${email}`
+    })
+}).then(response => response.json())
+.then(data => appendId(data))
+.catch(function(error) {
+    alert("Error - Something went wrong.")
+    appendError(error)
+})
+}
+
+function appendId(data){
+    let pElem = document.createElement('p')
+    pElem.textContent = data.id
+    document.querySelector('body').append(pElem)
+}
+function appendError(error){
+    let pError = document.createElement('p')
+    pError.textContent = error.message
+    document.querySelector('body').append(pError)
+
+}
